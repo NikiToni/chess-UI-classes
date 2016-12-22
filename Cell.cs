f65a8d620222d524f0f0e85cb81c902435a15fc1
NikiToni <monotonoto@gmail.com>
2016-12-22 22:34:21 +0200

    initial structure
    For now it only can populate the figures on board and dragging is not
    properly working

---
+++ Cell.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_drag_test
+{
+    class Cell
+    {
+        private bool empty;
+        private Figure figureOnMe;
+
+        public Cell(bool empty)
+        {
+            this.empty = empty;
+        }
+
+        public Figure MyFigure
+        {
+            get { return figureOnMe; } //make sure you dont access the figure of a empty cell
+            set {
+                empty = false;
+                figureOnMe = value;
+            }
+        }
+
+        public bool Empty
+        {
+            get { return empty; }
+            set { empty = value; }
+        }
+
+    }
+}
---
+++ ChessLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess_drag_test
+{
+    static class ChessLogic
+    {
+        
+
+
+/*
+        public static Figure[] getFiguresOnTable()
+        {
+            Figure[] retFigArr;
+            int numbOfFigOnTable = 0;
+            
+            foreach (Figure figure in FiguresBox.Figures)
+            {
+                if (figure.OnTable)
+                {
+                    numbOfFigOnTable++;
+                }
+            }
+
+            retFigArr = new Figure[numbOfFigOnTable];
+
+            int i = 0;
+
+            foreach (Figure figure in FiguresBox.Figures)
+            {
+                if (figure.OnTable)
+                {
+                    retFigArr[i] = figure;
+                    i++;
+                }
+            }
+
+            return retFigArr;
+        }
+        */
+    }
+}
